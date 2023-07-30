935101f1a8ec2b3c70a81a825da978c958100aa3
Konrad <01158980@pw.edu.pl>
2023-07-30 13:39:44 +0200

    Function convert_to_string, simple documentation

--- src/md5_calc.c
+++ src/md5_calc.c
@@ -1,6 +1,6 @@
 #include "md5_calc.h"
 
-unsigned int compute_md5(const char* input, unsigned char digest[MD5_DIGEST_LENGTH]){
+unsigned int compute_md5(const char* input, unsigned char* digest){
     unsigned int written;
     EVP_MD_CTX* md5ctx = EVP_MD_CTX_new();
     EVP_DigestInit(md5ctx, EVP_md5());
@@ -8,6 +8,13 @@ unsigned int compute_md5(const char* input, unsigned char digest[MD5_DIGEST_LENG
     EVP_DigestFinal_ex(md5ctx, digest, &written);
     EVP_MD_CTX_free(md5ctx);
     return written;
+}
+
+void convert_to_string(const unsigned char* input, char* output){
+    char* ptr = &output[0];
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++){
+        ptr += sprintf(ptr, "%02X", output[i]);
+    }
 }
 
 void print_hash(const unsigned char* input){
--- src/md5_calc.h
+++ src/md5_calc.h
@@ -8,7 +8,31 @@
 #define MD5_DIGEST_LENGTH           16
 #define EVP_DIGEST_UPDATE_NUM_BYTES 64
 
-unsigned int compute_md5(const char* input, unsigned char digest[MD5_DIGEST_LENGTH]);
+/*
+ * Computes the MD5 hash of the given string
+ *
+ * input - the string to be hashed
+ * digest - buffer where the end result will be stored, MUST be at least 16 chars long
+ *
+ * Returns the number of bytes written - the length of the digest
+ */
+unsigned int compute_md5(const char* input, unsigned char* digest);
+
+/*
+ * https://stackoverflow.com/questions/6357031/how-do-you-convert-a-byte-array-to-a-hexadecimal-string-in-c
+ *
+ * Similar to print_hash, but converts to null-terminated string in memory instead of
+ * directly printing to stdout.
+ *
+ * input - md5 digest
+ * output - buffer to write to; for MD5 it MUST be at least 2 * MD5_DIGEST_LENGTH + 1 long
+ */
+void convert_to_string(const unsigned char* input, char* output);
+
+/*
+ * Prints created hash to stdout
+ * input - buffer with computed hash
+ */
 void print_hash(const unsigned char* input);
 
 #endif //TURBOACCELERATED_RAINBOW_TABLES_MD5_CALC_H
