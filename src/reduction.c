8d062c6b727854bff344988f3008e30f24132ede
Konrad <01158980@pw.edu.pl>
2023-08-04 14:42:29 +0200

    WIP reduce, char_to_ascii stub

--- src/reduction.c
+++ src/reduction.c
@@ -1,18 +1,34 @@
 #include "reduction.h"
 
 void reduce(const unsigned char* digest, char* output, const char* reduction_pattern){
+    const int BASE_10_IDENTIFIER = 10;
     const char PATTERN_DELIMITER = '_';
+    char pattern_copy[MD5_DIGEST_LENGTH];
+    unsigned char pattern_numbers[MD5_DIGEST_LENGTH];
 
     // Performing copy since strtok() modifies original string
-    char pattern_copy[MD5_DIGEST_LENGTH + 1];
-    strcpy(pattern_copy, reduction_pattern);
+    memcpy(pattern_copy, reduction_pattern, MD5_DIGEST_LENGTH);
 
     // Extract the first token
     char* token = strtok(pattern_copy, &PATTERN_DELIMITER);
-    // Loop through the string to extract all other tokens
-    int i = 0;
-    while( token != NULL ) {
-        printf( " %s\n", token ); //printing each token
+
+    // Converting first token to number, clang suggests strtol() instead of atoi()
+    pattern_numbers[0] = (unsigned char) strtol(token, NULL, BASE_10_IDENTIFIER);
+
+    // Loop through the string to extract all other tokens, starting from 1 since token 0 is extracted
+    int i = 1;
+    while (token != NULL){
+        if (i > MD5_DIGEST_LENGTH - 1) break;
         token = strtok(NULL, &PATTERN_DELIMITER);
+        pattern_numbers[i] = (unsigned char) strtol(token, NULL, BASE_10_IDENTIFIER);
+        i++;
     }
+
+
+}
+
+char unsigned_char_to_ascii(const unsigned char in){
+
+
+    return 0;
 }
