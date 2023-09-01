833aa407a976a39cc8e7435bf973efef8278fcb6
Konrad <01158980@pw.edu.pl>
2023-09-01 11:21:32 +0200

    Tests for string to MD5 conversion

--- tst/md5calc_test.cpp
+++ tst/md5calc_test.cpp
@@ -18,7 +18,7 @@ TEST(MD5CalcTest, ComputeMD5Test){
     EXPECT_TRUE(result == 0) << "String digest nr 1 not equal" << std::endl;
 }
 
-TEST(MD5CalcTest, ConvertToStringTest){
+TEST(MD5CalcTest, ConvertMD5ToStringTest){
     // 0xea7fc82b5937beff14a7cb274b96b76e
     const unsigned char test_digest[MD5_DIGEST_LENGTH] =
             {234, 127, 200, 43, 89, 55, 190, 255,
@@ -28,4 +28,27 @@ TEST(MD5CalcTest, ConvertToStringTest){
     convert_md5_to_string(test_digest, out, sizeof out);
     int result = strcmp(out, "0xEA7FC82B5937BEFF14A7CB274B96B76E");
     EXPECT_TRUE(result == 0);
+}
+
+TEST(MD5CalcTest, ConvertStringToMD5Test){
+    const char* string_hash = "0xEA7FC82B5937BEFF14A7CB274B96B76E";
+    unsigned char hash[MD5_DIGEST_LENGTH];
+
+    convert_string_to_md5(string_hash, hash, sizeof hash);
+    EXPECT_EQ(hash[0], 234);
+    EXPECT_EQ(hash[1], 127);
+    EXPECT_EQ(hash[2], 200);
+    EXPECT_EQ(hash[3], 43);
+    EXPECT_EQ(hash[4], 89);
+    EXPECT_EQ(hash[5], 55);
+    EXPECT_EQ(hash[6], 190);
+    EXPECT_EQ(hash[7], 255);
+    EXPECT_EQ(hash[8], 20);
+    EXPECT_EQ(hash[9], 167);
+    EXPECT_EQ(hash[10], 203);
+    EXPECT_EQ(hash[11], 39);
+    EXPECT_EQ(hash[12], 75);
+    EXPECT_EQ(hash[13], 150);
+    EXPECT_EQ(hash[14], 183);
+    EXPECT_EQ(hash[15], 110);
 }
