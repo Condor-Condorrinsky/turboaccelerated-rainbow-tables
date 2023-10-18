7bbc280b36601628245e1cdabe21d5ec0d8b0c82
Konrad <01158980@pw.edu.pl>
2023-10-18 14:28:18 +0200

    Removed pass_len parameter from TableMetadata

--- src/TableMetadata.h
+++ src/TableMetadata.h
@@ -5,14 +5,12 @@
 
 typedef struct TableMetadata{
     int chain_len;
-    int passwd_len;
     int charset;
 } TableMetadata;
 
-const int DEFAULT_CHAIN_LEN  = 32;
-const int DEFAULT_PASSWD_LEN = 8;
-const int DEFAULT_CHARSET    = DIGITS;
+const static int DEFAULT_CHAIN_LEN  = 32;
+const static int DEFAULT_CHARSET    = DIGITS;
 
-void init_TableMetadata(TableMetadata* t, int chain_l, int passwd_l, int chars);
+void init_TableMetadata(TableMetadata* t, int chain_l, int chars);
 
 #endif //TURBOACCELERATED_RAINBOW_TABLES_TABLEMETADATA_H
